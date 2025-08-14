5607d3c3bab4561b11d8fab0261b59b169755478
zhikangwang <wangzhikang@snowtech.com.cn>
2025-08-14 20:58:17 +0800

    初始化项目

---
+++ ztna-server-app/src/main/java/cn/glfs/ztna/ZTNAApplication.java
@@ -0,0 +1,11 @@
+package cn.glfs.ztna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZTNAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZTNAApplication.class, args);
+    }
+}
